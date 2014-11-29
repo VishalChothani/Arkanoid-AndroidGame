24363996ea0431b48a4d3bfd38ee3c3015f9f13e
vishalchothani <vishal.k.chothani@gmail.com>
2014-11-28 21:24:54 -0800

    Android Game Unity3D
    This game is developed in unity3D

---
+++ Bounce/src/com/VIshal/Bounce/UnityPlayerActivity.java
@@ -0,0 +1,8 @@
+package com.VIshal.Bounce;
+
+import com.unity3d.player.*;
+
+/**
+ * @deprecated Use UnityPlayerNativeActivity instead.
+ */
+public class UnityPlayerActivity extends UnityPlayerNativeActivity { }
